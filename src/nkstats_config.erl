2fbf6e15f4992a05b09948d5dd457c74b6e4bed3
Pedro Gutiérrez <pedrogutierrez@mac.com>
2017-07-27 11:40:47 +0200

    restored missing port and path settings

--- src/nkstats_config.erl
+++ src/nkstats_config.erl
@@ -1,11 +1,24 @@
 -module(nkstats_config).
--export([collectors/0, auto_start/0]).
+-export([collectors/0, 
+         auto_start/0,
+         port/0,
+         path/0]).
 
 collectors() ->
     get_env(collectors, []).
 
 auto_start() ->
     get_env(auto_start, true).
+
+port() -> 
+    get_env(port, 9399).
+
+
+path() -> 
+    get_env(path, "/metrics").
+
+
+
 
 get_env(Key, Default) ->
     case application:get_env(nkstats, Key) of
