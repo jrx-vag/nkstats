293b53435c37d5218bf2bd4163d3765ad0cc06b8
Pedro Gutiérrez <pedrogutierrez@mac.com>
2017-07-27 11:38:38 +0200

    restored missing auto_start setting

--- src/nkstats_config.erl
+++ src/nkstats_config.erl
@@ -1,8 +1,11 @@
 -module(nkstats_config).
--export([collectors/0]).
+-export([collectors/0, auto_start/0]).
 
 collectors() ->
     get_env(collectors, []).
+
+auto_start() ->
+    get_env(auto_start, true).
 
 get_env(Key, Default) ->
     case application:get_env(nkstats, Key) of
