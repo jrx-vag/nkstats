fa829f7d194b21e248017641e83dd96313757e6f
Pedro Gutiérrez <pedrogutierrez@mac.com>
2017-10-14 21:00:37 +0200

    nkservice plugin project layout

--- include/nkstats.hrl
+++ include/nkstats.hrl
@@ -17,15 +17,9 @@
 %% under the License.
 %%
 %% -------------------------------------------------------------------
--module(nkstats).
--export([parse_exporter/3]).
--export_type([exporter_id/0, exporter/0]).
--include("nkstats.hrl").
-
--type exporter_id() :: term().
--type exporter() :: #{ config => map() }.
-
--spec parse_exporter(nkservice:id(), map(), nklib_syntax:parse_opts()) ->
-    {ok, exporter(), [binary()]} | {error, term()}.
-parse_exporter(SrvId, Map, ParseOpts) ->
-    SrvId:nkstats_parse_exporter(Map, ParseOpts).
+-define(APP, nkstats).
+-define(LOG(Level, Txt, Args), lager:Level("nkstats "++Txt, Args)).
+-define(INFO(Txt, Args), ?LOG(info, Txt, Args)).
+-define(DEBUG(Txt, Args), ?LOG(debug, Txt, Args)).
+-define(ERROR(Txt, Args), ?LOG(error, Txt, Args)).
+-define(WARN(Txt, Args), ?LOG(warn, Txt, Args)).
--- src/nkstats_app.erl
+++ src/nkstats_app.erl
@@ -1,16 +1,51 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2017 Carlos Gonzalez Florido.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
 -module(nkstats_app).
 -behaviour(application).
 -export([start/2, stop/1]).
+-export([get_exporter/1, put_exporter/2, get/2]).
+-include("nkstats.hrl").
 
 start(_, _) ->
-    case nkstats_config:auto_start() of 
-        false -> 
-            nkstats_sup:start_link();
-        true ->
-            nkstats_server:start(),
-            nkstats_sup:start_link()
+    Syntax = #{exporters=> {list, map}},
+    case nklib_config:load_env(?APP, Syntax) of
+        {ok, _} ->
+            {ok, Vsn} = application:get_key(?APP, vsn),
+            ?INFO("v~s is starting", [Vsn]),
+            %nkstats_server:start(),
+            {ok, Pid} = nkstats_sup:start_link(),
+            {ok, Pid};
+        {error, Error} ->
+            error({syntax_error, Error})
     end.
 
 stop(_) ->
     ok.
 
+get_exporter(Id) ->
+    get({exporter, nklib_util:to_binary(Id)}, not_found).
+
+put_exporter(Id, exporter) ->
+    Ids = get(exporter_ids, []),
+    put(exporter_ids, nklib_util:store_value(nklib_util:to_binary(Id), Ids)),
+    put({exporter, Id}, exporter).
+
+get(Key, Default) ->
+    nklib_config:get(?APP, Key, Default).
